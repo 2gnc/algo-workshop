8618d0d32f47cdc48fa9d161307630dfcd813cfc
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-04-07 18:43:27 +0300

    переделала на класс

--- bubble.js
+++ bubble.js
@@ -1,31 +1,38 @@
 // реализация сортировки пузырьком
-const arr = [9, 15, 80, 2, 0, 5, 1, 8]; 
+const arr = [9, 100, 80, 14, 2, 0, 5, 1, 8];
+const arr2 = [09, 0, 880, 14, 2, 99, 5, 100, 80];
 
-const sortIter = (uns) => {
-  let needNext = false;
-  let steps = uns.length;
-  for (let i = 0; i < steps; i++) {
-    if (uns[i] > uns[i+1]) {
-      needNext = true;
-      const tmp = uns[i];
-      uns[i] = uns[i+1];
-      uns[i+1] = tmp;
+class BubbleSorter {
+
+  iteration(uns) {
+    let needNext = false;
+    let steps = uns.length;
+    for (let i = 0; i < steps; i++) {
+      if (uns[i] > uns[i+1]) {
+        needNext = true;
+        const tmp = uns[i];
+        uns[i] = uns[i+1];
+        uns[i+1] = tmp;
+      }
     }
+    return needNext;
   }
-  return needNext;
+
+  sort(array) {
+    let x;
+    let count = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      x = this.iteration(array);
+      count += 1;
+      if (!x) {
+        break;
+      }
+    }
+    console.log('результат: ', array, 'итераций: ', count);
+  }
+
 }
 
-const bubbleSort = (array) => {
-  let x;
-  let count = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    x = sortIter(array);
-    count += 1;
-    if (x === false) {
-      break;
-    }
-  }
-  console.log('результат: ', array, 'итераций: ', count);
-}
-
-bubbleSort(arr);
+const sorter = new BubbleSorter();
+sorter.sort(arr);
+sorter.sort(arr2);
