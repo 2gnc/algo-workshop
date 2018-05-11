ec6b1cef8541d0bdb4eebaf433797a0799665561
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-05-11 15:59:20 +0300

    добавила третий вариант сортировки

--- selectionSort.js
+++ selectionSort.js
@@ -27,19 +27,46 @@
     let lastUnsortedIdx = res.length - 1;
     while (lastUnsortedIdx >= 0) {
       let min = Infinity;
+      let minI = null;
       for (let i = 0; i <= lastUnsortedIdx; i++) {
         if (res[i] < min) {
           min = res[i];
+          minI = i;
         }
       }
-      res.splice(Infinity, 0, res[res.indexOf(min)]);
-      res.splice(res.indexOf(min), 1);
+      res.splice(Infinity, 0, res[minI]);
+      res.splice(minI, 1);
       lastUnsortedIdx --;
     }
     return res;
   }
   
   console.log(sort2(arr));
+
+  console.log('=== Сортировка выбором 3 ===');
+
+  const sort3 = (arr) => {
+    const res = [...arr];
+    const lastIdx = res.length - 1;
+    let currIdx = 0;
+    while (currIdx <= lastIdx) {
+      let min = Infinity;
+      let idx = null;
+      for (let i = currIdx; i <= lastIdx; i++) {
+        if (res[i] < min) {
+          min = res[i];
+          idx = i;
+        }
+      }
+      const tmp = res[currIdx];
+      res[currIdx] = min;
+      res[idx] = tmp;
+      currIdx ++;
+    }
+    return res;
+  }
+  
+  console.log(sort3(arr));
 
   console.log('тесты sort');
   const t1 = [1, 4, 2, 3];
@@ -87,11 +114,38 @@
   };
 
   const x5 = [3, 2, 1, 4];
-  const x6 = sort(x5);
+  const x6 = sort2(x5);
   if (expect(x6[0]).toBe(1) &&
   expect(x6[1]).toBe(2) &&
   expect(x6[2]).toBe(3) &&
   expect(x6[3]).toBe(4)) {
+    console.log('тест 3 пройден');
+  };
+
+  console.log('тесты sort3');
+  const z1 = [1, 4, 2, 3];
+  const z2 = sort3(z1);
+  if (expect(z2[0]).toBe(1) &&
+  expect(z2[1]).toBe(2) &&
+  expect(z2[2]).toBe(3) &&
+  expect(z2[3]).toBe(4)) {
+    console.log('тест 1 пройден');
+  }; 
+
+  const z3 = [0, 0, 0];
+  const z4 = sort3(z3);
+  if (expect(z4[0]).toBe(0) &&
+  expect(z4[1]).toBe(0) &&
+  expect(z4[2]).toBe(0)) {
+    console.log('тест 2 пройден');
+  };
+
+  const z5 = [3, 2, 1, 4];
+  const z6 = sort3(z5);
+  if (expect(z6[0]).toBe(1) &&
+  expect(z6[1]).toBe(2) &&
+  expect(z6[2]).toBe(3) &&
+  expect(z6[3]).toBe(4)) {
     console.log('тест 3 пройден');
   };
 
